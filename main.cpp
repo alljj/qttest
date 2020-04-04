f47b5e9a40160f433fc90cf0ec31f909028ba40c
alljj <42672428+alljj@users.noreply.github.com>
2020-04-04 13:00:28 +0800

    主程序

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include "widget.h"
+#include "window.h"
+#include <QApplication>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+
+    w.desktopWnd = findDesktopIconWnd();
+
+    SendMessageToDesktop();
+
+      w.show();
+//      m.show();
+    return a.exec();
+}
