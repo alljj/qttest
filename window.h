93cbe1d6fba223e3ceb5cb1673ac1ca8931fd533
alljj <42672428+alljj@users.noreply.github.com>
2020-04-04 13:01:24 +0800

    Add files via upload

---
+++ window.h
@@ -0,0 +1,10 @@
+﻿#ifndef WINDOW_H
+#define WINDOW_H
+#include <windows.h>
+
+HWND findDesktopIconWnd();//找到桌面句柄
+void SendMessageToDesktop();//调用主窗口
+HWND GetWorkerW();//得到主窗口
+
+
+#endif // WINDOW_H
